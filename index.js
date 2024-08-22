edfbc331f37df8ad36f19a79c91d9ae02088c373
bhagat-abhishek <inst.abhishek@gmail.com>
2024-08-22 17:29:17 +0530

    handle post route

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.json({
-        "status": "success",
-        "msg": "Welcome to site"
-    })
+  res.json({
+    status: "success",
+    msg: "Welcome to site",
+  });
 });
 
+app.post("/", async (req, res) => {
+
+  console.log(req.body);
+
+  res.status(200).json({ message: "success" });
+});
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`);
